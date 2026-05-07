07c801e9fdaaaaa710b3193b32b8d44d414a613a
nafybbk <taby.bbk@gmail.com>
2026-05-07 20:07:08 +0530

    chore: update preload.js

---
+++ artifacts/desktop/electron/preload.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("bizcorDesktop", {
+  getTrialStatus: () => ipcRenderer.invoke("get-trial-status"),
+  getServerInfo: () => ipcRenderer.invoke("get-server-info"),
+  openInBrowser: (url) => ipcRenderer.invoke("open-in-browser", url),
+  onStatusUpdate: (fn) => ipcRenderer.on("status-update", (_, data) => fn(data)),
+});
